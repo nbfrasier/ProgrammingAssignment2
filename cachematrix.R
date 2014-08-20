1df8aa83b0d37b29433128a314e912901be88702
nbfrasier <nbfrasier@users.noreply.github.com>
2014-08-20 11:15:46 -0400

    Solved
    Ready for peer evaluation.

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-##The following function calculates the mean of the special "vector" created with the above function.
+## The following function calculates the mean of the special "vector" created with the above function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
